f8533a7f4d6f8d60a9e544e842e84fa0cda2d1af
hello1321 <41633577+hello1321@users.noreply.github.com>
2019-04-05 00:04:32 +0400

    Add files via upload

---
+++ c--master/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName="Nasa";
+    int characterAge;
+    characterAge = 50;
+    cout << characterName << " called me" << endl;
+    cout << characterName << " said don't hack us "<<characterAge<< " times" << endl;
+    cout << "but I did not listen to them " << endl;
+    cout << "I hacked "<<characterName<<" "<<characterAge<< " times" << endl;
+
+    return 0;
+}
